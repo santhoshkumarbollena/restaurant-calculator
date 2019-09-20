2689c180d576c7cd981f3b6b8bfe94a7ad55dbdb
pk <pk@pks-MacBook-Pro.local>
2019-09-20 03:12:36 -0500

    Restaurant calculator is done

--- JavaScript/calculate.js
+++ JavaScript/calculate.js
@@ -1,14 +1,13 @@
-
-document.getElementById("calculate").addEventListener('click',claculate)
-function claculate(){
-    let costInString=document.getElementById("cost");
-    let tipInString=document.getElementById("rate");
-
-    let cost=parseFloat(costInString)
-    let tip=parseFloat(tipInString)
-    console.log(cost+tip+5.50)
-    document.getElementById("grandtotal").innerHTML=(cost+tip+5.50)
-
-}
-
-
+document.querySelector('#calculate').addEventListener('click',function(){
+    const CostInString=document.querySelector('#cost').value;
+     const CostInFloat=parseFloat(CostInString);
+     const RateInString=document.querySelector('#rate').value;
+     const RateInFloat=parseFloat(RateInString);
+     const TipCalculation=CostInFloat*(RateInFloat/100)
+     const TaxRate=5.5
+     const Tax=CostInFloat*(TaxRate/100)
+     const GrandTotal=CostInFloat+TipCalculation+Tax;
+     document.querySelector('#TipValue').innerHTML=`$${TipCalculation.toFixed(2)}`
+     document.querySelector('#TaxValue').innerHTML=`$${Tax.toFixed(2)}`
+     document.querySelector('#TotalValue').innerHTML=`$${GrandTotal.toFixed(2)}`
+ })
